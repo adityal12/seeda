7df4c66cb049b155584e1efbc060a1ba7fbf0329
adityal12 <permanant064@gmail.com>
2024-10-16 08:38:30 +0530

    zxscdc
    asfad

---
+++ config.py
@@ -0,0 +1,22 @@
+# put yours
+API_ID =27117647
+API_HASH ="65aa269a28d39363dc595218b589b805"
+
+API_ID =10624990
+API_HASH ="5095d7291c67bac4d927130cd4ba7ee6"
+
+API_ID =22307231
+API_HASH ="a885cc372401b491c34ed1f7cc747b8b"
+
+# =============[upgrades]================
+# default is 5 level you can change it your self
+PAINT_REWARD_MAX = 5 # max is 7
+ENERGY_LIMIT_MAX = 5 # max is 6
+RE_CHARGE_SPEED_MAX = 5 # max is 11
+
+# ================[proxy]================
+USE_PROXY = False # or put True if you want use it
+PROXIES = {
+    "http":"socks5://127.0.0.1",
+    "https":"socks5://127.0.0.1", # if you using socks4 or http only replace it with "socks5"
+}
