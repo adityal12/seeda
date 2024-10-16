fdaa0f1499c1ecf640ae00f0714915260b94d292
adityal12 <permanant064@gmail.com>
2024-10-16 09:34:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,5 @@
 # put yours
-API_ID =27117647
-API_HASH ="65aa269a28d39363dc595218b589b805"
 
-API_ID =10624990
-API_HASH ="5095d7291c67bac4d927130cd4ba7ee6"
-
-API_ID =22307231
-API_HASH ="a885cc372401b491c34ed1f7cc747b8b"
 
 # =============[upgrades]================
 # default is 5 level you can change it your self
